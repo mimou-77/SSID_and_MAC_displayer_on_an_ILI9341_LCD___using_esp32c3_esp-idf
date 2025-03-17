b21c7307c72dee09d69e0ab0d99f664ef693db2c
mariem benhassine <mariem.benhassine79@gmail.com>
2025-03-17 15:14:58 +0100

    devices added to ok_list and nok_list (deletion part at spiffs_init() that will be changed at the final version ; il reste la partie web interface to send the csv files to a pc via wifi ; AP = esp32c3 , sta = pc

--- main/lists_lib.c
+++ main/lists_lib.c
@@ -19,6 +19,41 @@ void init_spiffs()
 
     esp_err_t err = esp_vfs_spiffs_register(&conf);
 
+    //change later
+    //delete content of ok_list.csv and nok_list.csv
+    FILE * ok_file_d = fopen("/spiffs/ok_list.csv", "w");
+    fclose(ok_file_d);
+    ESP_LOGI(TAG, "file content of ok_lists.csv deleted");
+    FILE * nok_file_d = fopen("/spiffs/nok_list.csv", "w");
+    fclose(nok_file_d);
+    ESP_LOGI(TAG, "file content of nok_lists.csv deleted");
+    //change later
+    //show content of ok_list.csv and nok_list.csv
+    FILE * ok_file_l = fopen("/spiffs/ok_list.csv", "r");
+        if (ok_file_l == NULL)
+        {
+            ESP_LOGI(TAG, "failed to open file: %s", ok_file_l);
+            return;
+        }
+    char line_ok[256]; //1 line of the file = 256 characters (max length)
+    while(fgets(line_ok, sizeof(line_ok), ok_file_l) != NULL)
+    {
+        ESP_LOGI(TAG, "csv line of ok_list.csv : %s", line_ok);
+    }
+    fclose(ok_file_l);
+    FILE * nok_file_l = fopen("/spiffs/nok_list.csv", "r");
+    if (nok_file_l == NULL)
+    {
+        ESP_LOGI(TAG, "failed to open file: %s", nok_file_l);
+        return;
+    }
+    char line_nok[256]; //1 line of the file = 256 characters (max length)
+    while(fgets(line_nok, sizeof(line_nok), nok_file_l) != NULL)
+    {
+        ESP_LOGI(TAG, "csv line of ok_list.csv : %s", line_nok);
+    }
+    fclose(nok_file_l);
+        
     size_t total = 0, used = 0;
     err = esp_spiffs_info(NULL, &total, &used); //get total and used
     if (err != ESP_OK)
@@ -42,8 +77,25 @@ void update_lists()
             return;
         } 
 
-        fprintf(ok_file, supla_device_mac);
+        fprintf(ok_file, "%s\n", supla_device_mac);
         fclose(ok_file);
+
+        //log file content (file = ok_list.csv)
+        FILE * ok_file_l = fopen("/spiffs/ok_list.csv", "r");
+        if (ok_file_l == NULL)
+        {
+            ESP_LOGI(TAG, "failed to open file: %s", ok_file_l);
+            return;
+        }
+        
+        char line[256]; //1 line of the file = 256 characters (max length)
+        int i = 0;
+        while(fgets(line, sizeof(line), ok_file_l) != NULL)
+        {
+            ESP_LOGI(TAG, "ok_list.csv line %d: %s", i, line);
+            i++;
+        }
+        fclose(ok_file_l);
     }
 
     if(btn_nok_pressed) //add device mac to ok_list
@@ -55,7 +107,24 @@ void update_lists()
             return;
         } 
 
-        fprintf(nok_file, supla_device_mac);
+        fprintf(nok_file, "%s\n", supla_device_mac);
         fclose(nok_file);
+
+        //log file content (file = nok_list.csv)
+        FILE * nok_file_l = fopen("/spiffs/nok_list.csv", "r");
+        if (nok_file_l == NULL)
+        {
+            ESP_LOGI(TAG, "failed to open file: %s", nok_file_l);
+            return;
+        }
+        
+        char line[256]; //1 line of the file = 256 characters (max length)
+        int i = 0;
+        while(fgets(line, sizeof(line), nok_file_l) != NULL)
+        {
+            ESP_LOGI(TAG, "nok_list.csv line %d: %s", i, line);
+            i++;
+        }
+        fclose(nok_file_l);
     }
 }
